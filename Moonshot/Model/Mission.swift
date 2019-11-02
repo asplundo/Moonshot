75c43bdf92038840b80b19d561688e520a0929e7
Olle Asplund <asplundo@Olles-MacBook-Pro-2.local>
2019-11-02 11:16:52 +0100

    Project 8 - 100DaysOfSwiftUI - Initial commit

---
+++ Moonshot/Model/Mission.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    var displayName: String {
+        return "Apollo \(id)"
+    }
+    
+    var image: String {
+        return "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        if let launchDate = launchDate {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            return formatter.string(from: launchDate)
+        } else {
+            return "N/A"
+        }
+    }
+}
